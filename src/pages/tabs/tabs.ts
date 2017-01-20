266caff65acf46e87d4a0660ec336a2ba1e83c1a
EIT-20160903KGQ\Administrator <zhengjc_1981@163.com>
2017-01-20 17:55:01 +0800

    no message

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+import { NavParams } from 'ionic-angular';
+
+import { AboutPage } from '../about/about';
+import { MapPage } from '../map/map';
+import { SchedulePage } from '../schedule/schedule';
+import { MainPage } from '../main/main';
+
+import { SpeakerListPage } from '../speaker-list/speaker-list';
+
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+  // set the root pages for each tab
+  tab0Root: any = MainPage;
+  tab1Root: any = SchedulePage;
+  tab2Root: any = SpeakerListPage;
+  tab3Root: any = MapPage;
+  tab4Root: any = AboutPage;
+  mySelectedIndex: number;
+
+  constructor(navParams: NavParams) {
+    this.mySelectedIndex = navParams.data.tabIndex || 0;
+  }
+
+}

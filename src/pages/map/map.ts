718f8042b6fd6e5aa133995c8c0a353050fd0182
EIT-20160903KGQ\Administrator <zhengjc_1981@163.com>
2017-03-03 17:34:38 +0800

    no message

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -3,26 +3,64 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { ConferenceData } from '../../providers/conference-data';
 import { Platform, NavController } from 'ionic-angular';
 import { SessionDetailPage } from '../session-detail/session-detail';
-
+import { Hero } from '../../providers/Hero';
+import {  Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
 })
+
+
 export class MapPage {
 
   @ViewChild('mapCanvas') mapElement: ElementRef;
-  constructor(public navCtrl: NavController,public confData: ConferenceData, public platform: Platform) {
-	      
-
+  private heroesUrl = '/api/category';
+  public categorys: Hero[];
+  constructor(private http: Http,public navCtrl: NavController,public confData: ConferenceData, public platform: Platform) {
+	   // this.http.get(this.heroesUrl).subscribe(res=> this.categorys =res.json());
   }
 
   ionViewDidLoad() {
-
-     
+		this.getHeroes();
 
   }
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then((rsp) => {
+					this.categorys=rsp.json();
+					for (let item of this.categorys){
+						if (item.children!=null){
+						item.grid = Array(Math.ceil(item.children.length/3));
+						let rowNum = 0; //counter to iterate over the rows in the grid
+						  for (let i = 0; i < item.children.length; i+=3) { //iterate images
+							item.grid[rowNum] = Array(3); //declare two elements per row
+
+							if (item.children[i]) { //check file URI exists
+							  item.grid[rowNum][0] = item.children[i] //insert image
+							}
+							if (item.children[i+1]) { //repeat for the second image
+							  item.grid[rowNum][1] = item.children[i+1]
+							}
+							if (item.children[i+2]) { //repeat for the three image
+							  item.grid[rowNum][2] = item.children[i+2]
+							}
+							rowNum++; //go on to the next row
+						  }
+					}}
+					console.log(this.categorys);
+				})
+       .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+  
   goToSessionDetail(sessionData) {
     // go to the session detail page
     // and pass in the session data
--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -1,17 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { AlertController, App, ItemSliding, List, ModalController, NavController, LoadingController } from 'ionic-angular';
 
-/*
-  To learn how to use third party libs in an
-  Ionic app check out our docs here: http://ionicframework.com/docs/v2/resources/third-party-libs/
-*/
-// import moment from 'moment';
-
-import { ConferenceData } from '../../providers/conference-data';
-import { ScheduleFilterPage } from '../schedule-filter/schedule-filter';
-import { SessionDetailPage } from '../session-detail/session-detail';
-import { UserData } from '../../providers/user-data';
 
 
 @Component({
